23946801791f4228e13955412b91f22548f2fe6a
Dor Alt <doralt@localhost.localdomain>
2018-04-20 20:59:17 +0300

    updated results

--- litl-master/runtests.bash
+++ litl-master/runtests.bash
@@ -37,41 +37,36 @@ done
 
 
 function run2(){
-	./lib$j.sh phoenix/tests/kmeans/kmeans-pthread -d 1000 -c 100 -p 9000 -s 2000
+	./lib$j.sh phoenix/tests/kmeans/kmeans-pthread -d 1000 -c 100 -p 9000 -s 2000 | grep 'time'
 }
 
 function run3(){
-	./lib$j.sh phoenix/tests/word_count/word_count-pthread word_count_datafiles/word_100MB.txt
+	./lib$j.sh phoenix/tests/word_count/word_count-pthread word_count_datafiles/word_100MB.txt | grep 'time'
 }
 
 function run4(){
-	./lib$j.sh phoenix/tests/pca/pca-pthread -r 1000 -c 100 -s 2000
+	./lib$j.sh phoenix/tests/pca/pca-pthread -r 1000 -c 100 -s 2000 | grep 'time' 
 }
 
 function run5(){
-	./lib$j.sh phoenix/tests/linear_regression/linear_regression-pthread linear_regression_datafiles/key_file_100MB.txt
+	./lib$j.sh phoenix/tests/linear_regression/linear_regression-pthread linear_regression_datafiles/key_file_100MB.txt | grep 'time'
 }
 
 function run6(){
-	./lib$j.sh phoenix/tests/string_match/string_match-pthread string_match_datafiles/key_file_100MB.txt
+	./lib$j.sh phoenix/tests/string_match/string_match-pthread string_match_datafiles/key_file_100MB.txt | grep 'time'
 }
-rm results_kmeas &> /dev/null
 for j in $algorithms;do
-	echo "********************\nalgo: $j, number of threads: $i,\n********************\n $(run2) \n\n" >> results_kmeas 
+	echo "algo: $j, kmeans $(run2)" >> results 
 done
-rm results_word_count &> /dev/null
 for j in $algorithms;do
-	echo "********************\nalgo: $j, number of threads: $i,\n********************\n $(run3) \n\n" >> results_word_count
+	echo "algo: $j, word_count $(run3)" >> results
 done
-rm results_pca &> /dev/null
 for j in $algorithms;do
-	echo "********************\nalgo: $j, number of threads: $i,\n********************\n $(run4) \n\n" >> results_pca
+	echo "algo: $j, PCA $(run4)" >> results
 done
-rm results_linear_regression &> /dev/null
 for j in $algorithms;do
-	echo "********************\nalgo: $j, number of threads: $i,\n********************\n $(run5) \n\n" >> results_linear_regression
+	echo "algo: $j, linear_regression $(run5)" >> results
 done
-rm results_string_match &> /dev/null
 for j in $algorithms;do
-	echo "********************\nalgo: $j, number of threads: $i,\n********************\n $(run6) \n\n" >> results_string_match
+	echo "algo: $j, string_match $(run6)" >> results
 done
